6556158490ae482211530518d099dadf7c8ccda9
ari.ira17-mcb <ari.ira17@yandex.ru>
2026-02-18 10:33:52 +0600

    feat: add dunders for bloom filter

--- bloom_filter/algorithm.py
+++ bloom_filter/algorithm.py
@@ -53,6 +53,30 @@ class BloomFilter:
                 return False
                 
         return True
+    
+
+    def __add__(self, other):
+
+        if self.m == other.m and self.k == other.k:
+            union = BloomFilter(m=self.m, k=self.k)
+            union.bit_array = [False] * union.m
+
+            for i in range (union.m):
+                union.bit_array[i] = self.bit_array[i] or other.bit_array[i]
+
+            return union
+    
+    
+    def __sub__(self, other):
+
+        if self.m == other.m and self.k == other.k:
+            crossing = BloomFilter(m=self.m, k=self.k)
+            crossing.bit_array = [False] * crossing.m
+
+            for i in range(crossing.m):
+                crossing.bit_array[i] = self.bit_array[i] and other.bit_array[i]
+
+            return crossing
 
 
 class BloomFilterCount(BloomFilter):
@@ -88,4 +112,27 @@ class BloomFilterCount(BloomFilter):
         for hash_f in self.hash_functions:
             value = hash_f.new_hash_f(element)
             index = value % self.m
-            self.counter_array[index] = max(0, self.counter_array[index] - 1)   
+            self.counter_array[index] = max(0, self.counter_array[index] - 1)
+
+
+    def __add__(self, other):
+
+        if self.m == other.m and self.k == other.k:
+            union = BloomFilterCount(m=self.m, k=self.k)
+            union.counter_array = [0] * union.m
+
+            for i in range (union.m):
+                union.counter_array[i] = max(self.counter_array[i], other.counter_array[i])
+
+            return union
+        
+    def __sub__(self, other):
+
+        if self.m == other.m and self.k == other.k:
+            crossing = BloomFilterCount(m=self.m, k=self.k)
+            crossing.counter_array = [0] * crossing.m
+
+            for i in range (crossing.m):
+                crossing.counter_array[i] = min(self.counter_array[i], other.counter_array[i])
+
+            return crossing
--- bloom_filter/tests.py
+++ bloom_filter/tests.py
@@ -76,6 +76,7 @@ def bloom_filter_test_str(n, eps, num_of_attemps, fill_percent=0.25):
 
     return avg_rate
 
+# 3
 
 n = 15_000
 eps = 0.5 
@@ -90,3 +91,31 @@ for fill in [0.25, 0.5, 0.75, 0.95]:
     bloom_filter_test_str(n=15_000, eps=0.5, num_of_attemps=5, fill_percent=fill)
 
 print(f"\n m = {int(m)}, k = {(k)}, eps = {round(eps, 3) * 100}%")
+
+
+# 6
+def test_union(bloom_filter_1, bloom_filter_2):
+    union = bloom_filter_1 + bloom_filter_2
+    for el in ['1', '2', '4', '5']:
+        assert union.check(el) == True
+    assert union.check('8') == False
+
+
+def test_crossing(bloom_filter_1, bloom_filter_2):
+    crossing = bloom_filter_1 - bloom_filter_2
+    assert crossing.check('4')
+    for el in ['1', '2', '5']:
+        assert crossing.check(el) == False
+
+
+bf_1 = BloomFilter(m=100, k=5)
+bf_1.add('1')   
+bf_1.add('2')  
+bf_1.add('4') 
+
+bf_2 = BloomFilter(m=100, k=5)
+bf_2.add('4')   
+bf_2.add('5')  
+
+test_union(bf_1, bf_2)
+test_crossing(bf_1, bf_2)
