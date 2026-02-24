0ad4858d3f766012a3870660f0facf80b30aa61d
ari.ira17-mcb <ari.ira17@yandex.ru>
2026-02-24 19:43:07 +0600

    test: fix bloom filter  tests

--- bloom_filter/tests.py
+++ bloom_filter/tests.py
@@ -1,3 +1,4 @@
+import math
 from algorithm import BloomFilter, BloomFilterCount
 
 def test_builder_bf():
@@ -6,8 +7,10 @@ def test_builder_bf():
     assert bf_1.k == 5
 
     bf_2 = BloomFilter(n=1000, eps=50)
-    assert bf_2.m == 1000
-    assert bf_2.k == 50
+    true_m = int( - (bf_2.n * math.log(bf_2.eps)) / (math.log(2) ** 2))
+    true_k = max(1, int((bf_2.m / bf_2.n) * math.log(2)))
+    assert round(bf_2.m, 10) == round(true_m, 10)
+    assert round(bf_2.k, 10) == round(true_k, 10)
 
 
 def test_builder_bf_counter():
@@ -16,8 +19,10 @@ def test_builder_bf_counter():
     assert bf_counter_1.k == 5
 
     bf_counter_2 = BloomFilter(n=1000, eps=50)
-    assert bf_counter_2.m == 1000
-    assert bf_counter_2.k == 50
+    true_m = int( - (bf_counter_2.n * math.log(bf_counter_2.eps)) / (math.log(2) ** 2))
+    true_k = max(1, int((bf_counter_2.m / bf_counter_2.n) * math.log(2)))
+    assert round(bf_counter_2.m, 10) == round(true_m, 10)
+    assert round(bf_counter_2.k, 10) == round(true_k, 10)
 
 
 def test_add_check_bf():
@@ -42,7 +47,7 @@ def test_remove_bf_counter():
     assert bf_counter.check('cat') == False 
 
 
-def test_union_bf(bf_1, bf_2):
+def test_union_bf():
 
     bf_1 = BloomFilter(m=100, k=5)
     bf_1.add('1')   
@@ -61,7 +66,15 @@ def test_union_bf(bf_1, bf_2):
     assert union.check('8') == False
 
 
-def test_union_bf_counter(bf_1, bf_2):
+def test_union_bf_counter():
+    bf_1 = BloomFilterCount(m=200, k=7)
+    bf_1.add('cat')   
+    bf_1.add('bird')  
+    bf_1.add('dog') 
+
+    bf_2 = BloomFilterCount(m=200, k=7)
+    bf_2.add('dog')   
+    bf_2.add('cow')
 
     union_counter = bf_1 + bf_2
 
@@ -88,7 +101,7 @@ def test_crossing_bf():
         assert crossing.check(el) == False
 
 
-def test_crossing_bf_counter(bf_1, bf_2):
+def test_crossing_bf_counter():
 
     bf_counter_1 = BloomFilterCount(m=200, k=7)
     bf_counter_1.add('cat')   
@@ -99,7 +112,18 @@ def test_crossing_bf_counter(bf_1, bf_2):
     bf_counter_2.add('dog')   
     bf_counter_2.add('cow')
 
-    crossing = bf_1 - bf_2
+    crossing = bf_counter_1 - bf_counter_2
     assert crossing.check('dog')
     for el in ['cat', 'bird', 'cow']:
         assert crossing.check(el) == False
+
+
+test_builder_bf()
+test_builder_bf_counter()
+test_add_check_bf()
+test_add_check_bf_counter()
+test_remove_bf_counter()
+test_union_bf()
+test_union_bf_counter()
+test_crossing_bf()
+test_crossing_bf_counter()
