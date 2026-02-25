2552bbd29ab804561f8f331547fb53d167d6cb25
ari.ira17-mcb <ari.ira17@yandex.ru>
2026-02-25 09:33:21 +0600

    fix: plots

--- bloom_filter/analysis.py
+++ bloom_filter/analysis.py
@@ -1,4 +1,4 @@
-from tests import generate_items
+from false_positive_rate import generate_items
 from algorithm import BloomFilter
 
 import matplotlib.pyplot as plt
@@ -23,22 +23,21 @@ def experiment(m, k, attemps, num_of_elements):
     return false_positive / (attemps * num_of_elements)
 
 
-m = [100, 200, 500, 800, 1000]
-k = [x for x in range(1, 10)]
+m_values = [100, 200, 500, 800, 1000]
+k_values = [x for x in range(1, 10)]
 attemps = 100
 num_of_elements = 100
 
 results = []
 
-for m_value in m:
-    for k_value in k:
-        res = experiment(m_value, k_value, attemps, num_of_elements)
-        results.append({'m': m_value, 'k': k_value, 'res': res})
+for m in m_values:
+    for k in k_values:
+        res = experiment(m, k, attemps, num_of_elements)
+        results.append({'m': m, 'k': k, 'Результат': res})
 
 df = pd.DataFrame(results)
 
-fig, ax = plt.subplots(figsize=(9, 9))
-ax.axis('tight')
+fig, ax = plt.subplots(figsize=(10, 15))
 ax.axis('off')
 
 table = ax.table(
@@ -47,12 +46,18 @@ table = ax.table(
     loc='center',
     cellLoc='center'
 )
+
+table.auto_set_font_size(False)
+table.set_fontsize(12)    
+table.scale(0.5, 1.5)       
+plt.title('Зависимость вероятности от числа хеш-функций', fontsize=20, pad=20)
+
 plt.savefig('bloom_filter/empirical_probability_of_a_false_positive_table.png')
 
 plt.figure(figsize=(10, 6))
-for m_v in m:
-    subset = df[df["m"] == m_v]
-    plt.plot(subset["k"], subset["res"], marker='o', label=f"m = {m_v}")
+for m in m_values:
+    subset = df[df["m"] == m]
+    plt.plot(subset["k"], subset["Результат"], label=f"m = {m}")
 
 plt.xlabel("Число хеш-функций (k)")
 plt.ylabel("Эмпирическая вероятность ложноположительного срабатывания")
