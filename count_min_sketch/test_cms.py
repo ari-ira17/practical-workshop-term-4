c29638640422eaa65a0a9af157866112c44aaaac
ari.ira17-mcb <ari.ira17@yandex.ru>
2026-02-26 20:46:16 +0600

    test: add union test

--- count_min_sketch/test_cms.py
+++ count_min_sketch/test_cms.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import random
-from datetime import datetime
 from collections import Counter
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from hyperloglog.test_hll import date_gen
@@ -10,49 +9,63 @@ from count_min_sketch.cms_algorithm import CountMinSketch
 
 
 def test_cms_threads():
-    threads = [25_000, 200_000]
+    threads = [200_000, 1_000_000]
 
     for thread in threads:
         random.seed(thread / 1000)
-        cms = CountMinSketch(eps=0.01, delta=0.01)
-        gen = date_gen()
-        seen = []
+        runs = 5
+        errors = []
 
-        for i in range(thread):
-            date = next(gen)
-            cms.update(date)
-            seen.append(date)
+        for _ in range(runs):
+            cms = CountMinSketch(eps=0.01, delta=0.01)
+            gen = date_gen()
+            seen = []
+            for _ in range(thread):
+                date = next(gen)
+                cms.update(date)
+                seen.append(date)
 
-        # frequency = cms.frequency(datetime(2026, 2, 26))
-        true_freq = Counter(seen)
-        for key, value in true_freq.items():
-            frequency = cms.frequency(key)
-            rel_error = abs(value - frequency) / thread
-            print(f"{thread}: дата {key} добавлена {value} раз")
-            print(f"{thread}: оценка даты {key} составляет {frequency}")
-            print(f"Погрешность составялет {rel_error}")
+            true_freq = Counter(seen)
+            for key, value in true_freq.items():
+                frequency = cms.frequency(key)
+                abs_error = abs(value - frequency)
+                errors.append(abs_error)
 
-        assert abs(value - frequency) <= cms.eps * thread
+        avg_abs_error = sum(errors) / len(errors)
+        print(f"В потоке {thread} абсолютная погрешность составялет {round(avg_abs_error, 3)}")
+        assert sum(errors) / len(errors) <= cms.eps * thread
+
 
 def test_cms_union():
-    cms_1 = CountMinSketch(eps=0.01, delta=0.01)
-    cms_2 = CountMinSketch(eps=0.01, delta=0.01)
+    threads = [200_000, 1_000_000]
+    random.seed(sum(threads) / 1000)
+    runs = 5
+    errors = []
 
-    for _ in range(100):
-        cms_1.update("A")
-    for _ in range(200):
-        cms_2.update("A")
+    for _ in range(runs):
+        cms_1 = CountMinSketch(eps=0.01, delta=0.01)
+        cms_2 = CountMinSketch(eps=0.01, delta=0.01)
+        gen_1 = date_gen()
+        gen_2 = date_gen()
+        seen = []
 
-    union = cms_1 + cms_2
+        for _ in range(threads[0]):
+            date = next(gen_1)
+            cms_1.update(date)
+            seen.append(date)
 
-    estimated = union.frequency("A")
-    true_freq = 300
-    assert abs(estimated - true_freq) <= 0.01 * 300 
+        for _ in range(threads[1]):
+            date = next(gen_2)
+            cms_2.update(date)
+            seen.append(date)
 
+        cms_union = cms_1 + cms_2
+        true_freq = Counter(seen)
+        for key, value in true_freq.items():
+            frequency = cms_union.frequency(key)
+            abs_error = abs(value - frequency)
+            errors.append(abs_error)
 
-cms = CountMinSketch(eps=0.01, delta=0.01)
-data_stream = ["apple", "banana", "apple", "orange", "apple", "banana", "banana"]
-for el in data_stream:
-    cms.add(el)
-
-print("Frequency of 'apple':", cms.frequency("apple"))
+    avg_abs_error = sum(errors) / len(errors)
+    print(f"В объединении абсолютная погрешность составялет {round(avg_abs_error, 3)}")
+    assert sum(errors) / len(errors) <= cms_union.eps * sum(threads)
